715cb29eef21ef1a22e2eaf79ca7a61f96dfd5d8
NeVeSpl <nevespl@gmail.com>
2019-09-26 20:22:07 +0200

    Migrate TestMe.SharedKernel to .net core 3.0

--- TestMe.SharedKernel/Persistence/Extensions/Microsoft.EntityFrameworkCore/ModelBuilderExtensions .cs
+++ TestMe.SharedKernel/Persistence/Extensions/Microsoft.EntityFrameworkCore/ModelBuilderExtensions .cs
@@ -1,5 +1,4 @@
 ﻿using Microsoft.EntityFrameworkCore.Metadata;
-using Microsoft.EntityFrameworkCore.Metadata.Internal;
 
 namespace Microsoft.EntityFrameworkCore
 {
@@ -8,8 +7,14 @@ namespace Microsoft.EntityFrameworkCore
         public static void RemovePluralizingTableNameConvention(this ModelBuilder modelBuilder)
         {
             foreach (IMutableEntityType entity in modelBuilder.Model.GetEntityTypes())
-            {
-                entity.Relational().TableName = entity.DisplayName();
+            {               
+                // EF core 3.0 supports only one type of inheritence: table per hierarchy (TPH)
+                // thus we set table name only for root entity
+                if (entity.ClrType.BaseType.Name == "Object")
+                {
+                    string displayName = entity.DisplayName();                    
+                    entity.SetTableName(displayName);
+                }
             }
         }
     }
