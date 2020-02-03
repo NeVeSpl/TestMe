f8ab2f88f057b4980d5c12bffb645b4958a814f0
NeVeSpl <nevespl@gmail.com>
2020-02-03 19:37:43 +0100

    Extract ReadOnlyTestCreationDbContext class to separate file

---
+++ TestMe.TestCreation/Persistence/ReadOnlyTestCreationDbContext.cs
@@ -0,0 +1,30 @@
+﻿using Microsoft.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore.ChangeTracking;
+
+namespace TestMe.TestCreation.Persistence
+{
+    internal sealed class ReadOnlyTestCreationDbContext : TestCreationDbContext
+    {
+        /* Migration to EF core 3.0 - owned entites cannot be longer used also as Query Type/Keyless Entity Types
+        public DbSet<Answer> Answers
+        {
+            get; protected set;
+        }
+        public DbSet<QuestionsCatalogItem> QuestionsCatalogItems { get; protected set; }
+        public DbSet<QuestionItem> QuestionItems { get; protected set; }
+        */
+
+
+        public ReadOnlyTestCreationDbContext(DbContextOptions options) : base(options)
+        {
+            ChangeTracker.LazyLoadingEnabled = false;
+            ChangeTracker.AutoDetectChangesEnabled = false;
+            ChangeTracker.QueryTrackingBehavior = QueryTrackingBehavior.NoTracking;
+        }
+
+        protected override void OnModelCreating(ModelBuilder modelBuilder)
+        {
+            base.OnModelCreating(modelBuilder);
+        }
+    }   
+}
--- TestMe.TestCreation/Persistence/TestCreationDbContext.cs
+++ TestMe.TestCreation/Persistence/TestCreationDbContext.cs
@@ -36,29 +36,4 @@ namespace TestMe.TestCreation.Persistence
             modelBuilder.ApplyConfigurationsFromAssembly(Assembly.GetExecutingAssembly(), x => x.FullName?.StartsWith(configurationsPrefix!) ?? false);         
         }
     }
-
-    internal sealed class ReadOnlyTestCreationDbContext : TestCreationDbContext
-    {
-        /* Migration to EF core 3.0 - owned entites cannot be longer used also as Query Type/Keyless Entity Types
-        public DbSet<Answer> Answers
-        {
-            get; protected set;
-        }
-        public DbSet<QuestionsCatalogItem> QuestionsCatalogItems { get; protected set; }
-        public DbSet<QuestionItem> QuestionItems { get; protected set; }
-        */
-
-
-        public ReadOnlyTestCreationDbContext(DbContextOptions options) : base(options)
-        {
-            ChangeTracker.LazyLoadingEnabled = false;
-            ChangeTracker.AutoDetectChangesEnabled = false;
-            ChangeTracker.QueryTrackingBehavior = QueryTrackingBehavior.NoTracking;
-        }
-
-        protected override void OnModelCreating(ModelBuilder modelBuilder)
-        {
-            base.OnModelCreating(modelBuilder);
-        }
-    }   
 }
