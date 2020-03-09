f250f2859f79eec26ab34374fe073f0eafa10a5d
NeVeSpl <nevespl@gmail.com>
2020-03-09 18:35:50 +0100

    add types for react env variables

--- TestMe.Presentation.React/ClientApp/src/react-app-env.d.ts
+++ TestMe.Presentation.React/ClientApp/src/react-app-env.d.ts
@@ -1,1 +1,9 @@
 /// <reference types="react-scripts" />
+declare namespace NodeJS
+{
+    interface ProcessEnv
+    {
+        NODE_ENV: 'development' | 'production' | 'test';
+        REACT_APP_API_URL: string;    
+    }
+}
